502e3a277f96099144a587787c5f6dd64a2f9f04
Gabriel Kochem <gabikochem55@gmail.com>
2025-09-06 11:56:37 -0300

    To improved front end

--- FrontEnd_SGAgenda/src/layouts/MainLayout.vue
+++ FrontEnd_SGAgenda/src/layouts/MainLayout.vue
@@ -5,7 +5,7 @@
                 class="q-pa-md ml-16 text-xl"
             >     
                 <div class="flex" v-if="width as number >= 1100">
-                    <q-avatar rounded class="w-40 h-10 cursor-pointer">
+                    <q-avatar rounded class="w-40 h-10 cursor-pointer mr-10">
                         <img 
                             :src="surprise ? 'public/images/GK_agenda.png' : 'public/images/logo-branca.png'"
                             @click="surprise = !surprise"
@@ -38,47 +38,49 @@
                 </div>
 
                 <div v-else>
-                    <q-avatar rounded class="w-40 h-10 cursor-pointer">
-                        <img 
-                            :src="surprise ? 'public/images/GK_agenda.png' : 'public/images/logo-branca.png'"
-                            @click="surprise = !surprise"
-                        >
-                    </q-avatar>
-                    <q-btn 
-                        flat
-                        round
-                        icon="menu"
-                        class="ml-14"
-                        @click="show = !show"
-                    />
+                    <div class="flex justify-center">
+                        <q-avatar rounded class="w-40 h-10 cursor-pointer">
+                            <img 
+                                :src="surprise ? 'public/images/GK_agenda.png' : 'public/images/logo-branca.png'"
+                                @click="surprise = !surprise"
+                            >
+                        </q-avatar>
+                        <q-btn 
+                            flat
+                            round
+                            icon="menu"
+                            class="ml-14"
+                            @click="show = !show"
+                        />
+                    </div>
 
                     <q-dialog v-model="show" persistent>
                         <div class="bg-white p-4 flex-col text-center">
-                            <div class="ml-auto mr-auto hover:bg-blue-500 rounded-md p-1">
+                            <div class="">
+                                <q-btn flat icon="close" color="primary" class="" v-close-popup />
+                            </div>
+
+                            <q-btn 
+                                :style="`background-color: rgb(59 130 200 / var(--tw-bg-opacity, 1))`"
+                                class="mb-2 mt-2 flex justify-center text-white"
+                                label="Criar conta"
+                                
+                            />
+
+                            <q-btn 
+                                :style="`background-color: #fff`"
+                                class="text-blue-600 mt-2 mb-4"
+                                label="Entrar"
+                                
+                            />
+                            <div class="ml-auto mr-auto hover:bg-blue-500 hover:text-white rounded-md p-1">
                                 <router-link to="">Contato</router-link>
+                                
                             </div>
                             
-                            <div class="hover:bg-blue-500 rounded-md p-1">
+                            <div class="hover:bg-blue-500 hover:text-white rounded-md p-1">
                                 <router-link to="">Suporte</router-link>
                             </div>
-
-                            <div class="">
-                                <q-btn 
-                                    :style="`background-color: rgb(59 130 200 / var(--tw-bg-opacity, 1))`"
-                                    class="mb-4"
-                                    label="Criar conta"
-                                    
-                                />
-
-                                <q-btn 
-                                    :style="`background-color: #fff`"
-                                    class="text-blue-600 mb-4 flex"
-                                    label="Entrar"
-                                    
-                                />
-                            </div>
-                            
-                            <q-btn flat label="Cancel" color="primary" v-close-popup />
                         </div>
                     </q-dialog>
                 </div>
@@ -97,6 +99,6 @@
 
     const width = LocalStorage.getItem("width");
     let surprise = ref<boolean>(true);
-    let show = ref<boolean>(true);
+    let show = ref<boolean>(false);
 
 </script>
--- FrontEnd_SGAgenda/src/pages/IndexPage.vue
+++ FrontEnd_SGAgenda/src/pages/IndexPage.vue
@@ -1,8 +1,28 @@
 <template>
-    <h1>A</h1>
+    <q-page style="padding-top: 0px" class="q-pa-md">
+        <main>
+            <section id="section1">
+                <div class="p-12">
+                    <article class="mt-12 text-4xl">
+                        <span class="text">Agenda</span>
+                    </article>
+                </div>
+            </section>
+        </main>
+    </q-page>
 </template>
 
 <script setup lang="ts">
-    import { onMounted, ref } from 'vue';
-
 </script>
+
+<style lang="scss">
+    #section1 {
+        background-image: url('../../public/images/imagem_reuniao.jpg');
+        background-repeat: no-repeat; 
+        position: relative;
+        height: 200vh;
+        width: 100vh;
+        
+    }   
+
+</style>
